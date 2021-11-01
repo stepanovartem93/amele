f588645fa7878a66e3be7258c21d869cd4fc005e
stepanovartem93 <stepanovartem93@gmail.com>
2021-11-01 06:36:16 +0300

    -have made modify  of model manager

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -7,7 +7,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter= ('status', 'created', 'publish', 'author')
     search_fields  = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ('author',)
+    prepopulated_fields = {'slug': ('title',)} #автоматически формирует краткий заголовок
+    raw_id_fields = ('author',) #позволяет, при создании выбора автора заголовка не открывать выпадающий список, а выбрать в отдельном окне автора
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
